b652dc93deac505ecc086e57c288e9b7c4bb37e7
cyntiaaishimwe <ishimwecynthiaa@gmail.com>
2023-06-17 23:09:14 +0300

    to-do-list

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 
+fetch('https://dummyjson.com/todos')
+.then(res => res.json())
+.then(data => {
+  const todoListDiv = document.getElementById('priorities');
+
+  
+  data.forEach(todo => {
+    const todoItem = document.createElement('p');
+    todoItem.textContent = todo.title;
+    todoListDiv.appendChild(todoItem);
+  });
+})
+.catch(error => {
+  console.error('Error fetching API:', error);
+});
+
