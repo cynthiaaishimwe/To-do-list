a828003c3bcd9115e29fdd6db22e7cf1581699e6
cyntiaaishimwe <ishimwecynthiaa@gmail.com>
2023-06-18 21:28:55 +0300

    to-do-list

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-
+const highPriorityInputs = document.querySelectorAll('#high-priority input[type="text"]');
+const mediumPriorityInputs = document.querySelectorAll('#medium-priority input[type="text"]');
+const lowPriorityInputs = document.querySelectorAll('#low-priority input[type="text"]');
+const completedTaskInputs = document.querySelectorAll('#done input[type="text"]');
+const progressiveTaskInputs = document.querySelectorAll('#in-progress input[type="text"]');
 
 fetch('https://dummyjson.com/todos')
-.then(res => res.json())
-.then(data => {
-  const todoListDiv = document.getElementById('priorities');
+  .then(res => res.json())
+  .then(data => {
+    const highPriorityTodos = data.todos.filter(todo => todo.id >= 1 && todo.id <= 2);
+    const mediumPriorityTodos = data.todos.filter(todo => todo.id >= 3 && todo.id <= 4);
+    const lowPriorityTodos = data.todos.filter(todo => todo.id >= 5 && todo.id <= 6);
+    const completedTasks = data.todos.filter(todo => todo.completed === true);
+    const progressiveTasks = data.todos.filter(todo => todo.completed === false);
+    highPriorityInputs.forEach((input, index) => {
+      input.value = highPriorityTodos[index] ? highPriorityTodos[index].todo : '';
+    });
 
-  
-  data.forEach(todo => {
-    const todoItem = document.createElement('p');
-    todoItem.textContent = todo.title;
-    todoListDiv.appendChild(todoItem);
+    mediumPriorityInputs.forEach((input, index) => {
+      input.value = mediumPriorityTodos[index] ? mediumPriorityTodos[index].todo : '';
+    });
+
+    lowPriorityInputs.forEach((input, index) => {
+      input.value = lowPriorityTodos[index] ? lowPriorityTodos[index].todo : '';
+    });
+
+    completedTaskInputs.forEach((input, index) => {
+      input.value = completedTasks[index] ? completedTasks[index].todo : '';
+    });
+
+    progressiveTaskInputs.forEach((input, index) => {
+      input.value = progressiveTasks[index] ? progressiveTasks[index].todo : '';
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching API:', error);
   });
-})
-.catch(error => {
-  console.error('Error fetching API:', error);
-});
-
